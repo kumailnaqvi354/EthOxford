9c036cf99ef1a4370a7dcfbb11cd7bd7d0322f1c
kumailnaqvi354 <komailnaqvi616@gmail.com>
2025-02-08 01:19:43 +0000

    feat | init

---
+++ src/FLRToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.25;
+
+import {ERC20} from "@openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+
+contract FLRToken is ERC20{
+
+    constructor() ERC20("FLRToken", "FLT"){
+        _mint(address(this), type(uint256).max);
+    }
+
+}
